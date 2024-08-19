77b9383672a8621fbc1dbccfde876f9dd2368c4c
Nejimon Raveendran <nejimon.raveendran@gmail.com>
2024-08-19 00:48:40 -0400

    site updated

---
+++ site.js
@@ -0,0 +1,6 @@
+var mobilemenubutton = document.querySelector(".mobilemenubutton");
+var mobilemenu = document.querySelector(".mobilemenu");
+
+mobilemenubutton.addEventListener("click", function(){
+    mobilemenu.classList.toggle("showmobilemenu");
+})

3d5196ae703b653f90e48dfac4d3811f61238412
Nejimon Raveendran <nejimon.raveendran@gmail.com>
2024-10-06 18:41:48 -0400

    site updated

--- site.js
+++ site.js
@@ -1,7 +1,22 @@
 var mobilemenubutton = document.querySelector(".mobilemenubutton");
 var mobilemenu = document.querySelector(".mobilemenu");
+var deskemenuitems = document.querySelectorAll(".normalmenuwrapper nav ul li");
 
 mobilemenubutton.addEventListener("click", function(){
     mobilemenu.classList.toggle("showmobilemenu");
 })
 
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('menuanimation');
+            //animation: rotate_menu 0.5s ease-in-out 1;
+        }else{
+            entry.target.classList.remove('menuanimation');
+        }
+    });
+}, {threshold: 0.5});
+
+deskemenuitems.forEach(item => observer.observe(item));
+//observer.observe(deskemenuitem);
